89fb293142ffdafd1ba9d6fabe7fdbd2235f8f85
liubeibei <liubeibei@vipcode.com>
2019-08-15 10:30:56 +0800

    IU-2019.1.2 <liube@DESKTOP-EJ9IHME Create baseRefactoring.xml, markdown.xml, git.xml, jprofiler.xml, project.default.xml, Enum.java, laf.xml, Interface.java, lombok-plugin.xml, Class.java, jdk.table.xml, terminal.xml, Default.xml, path.macros.xml, find.xml, filetypes.xml, vcs.xml, colors.scheme.xml, toStringTemplates.xml, VisualVMLauncher.xml, mavenVersion.xml, debugger.xml, updates.xml

---
+++ fileTemplates/internal/Interface.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+* @InterfaceName: ${NAME}
+* @Description: TODO
+* @author: liubeibei
+* @date: ${YEAR}-${MONTH}-${DAY} ${HOUR}:${MINUTE}
+**/
+public interface ${NAME} {
+}
